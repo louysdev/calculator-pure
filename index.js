b0ca6f5323ec684eb37e601a3ff2724fc6aa30e8
LouysDev <louysdev@gmail.com>
2023-09-15 20:07:11 -0400

    Historial de la calculadora

--- index.js
+++ index.js
@@ -1,46 +1,91 @@
 // Obtener elementos del DOM
 const botones = document.getElementsByClassName("boton");
 const btn = document.getElementById("btn");
-const pantalla = document.getElementById("pantalla");
+const pantallaActual = document.getElementById("actuales");
+const pantallaAnterior = document.getElementById("anteriores");
 
-const valores = new Array(3);
+let operaciones = "";
+let resultado = "";
+let cantidadRepiticiones = 0;
 
 const agregarNumero = (number) => {
-  const dataAnterior = pantalla.innerHTML;
+  const dataAnterior = pantallaActual.innerHTML;
 
   if (dataAnterior.charAt(0) === "0") {
-    pantalla.innerHTML = "";
-    pantalla.innerHTML = `${number}`;
+    pantallaActual.innerHTML = "";
+    pantallaActual.innerHTML = `${number}`;
   } else {
-    pantalla.innerHTML = `${dataAnterior}${number}`;
+    pantallaActual.innerHTML = `${dataAnterior}${number}`;
   }
+};
+
+const detectarSignoRepetido = (cadena) => {
+  console.log(cadena);
+  const cadenaSeparada = cadena.split("");
+
+  for (let i = 0; i < cadenaSeparada.length; i++) {
+    if (cadenaSeparada[i] === "+") {
+      cantidadRepiticiones++;
+    }
+  }
+
+  const noPasable = cantidadRepiticiones > 1 ? true : false;
+  cantidadRepiticiones = cantidadRepiticiones >= 2 && 0;
+
+  return noPasable;
+};
+
+const limpiar = () => {
+  pantallaActual.innerHTML = "0";
+};
+
+const limpiarCompleto = () => {
+  pantallaActual.innerHTML = "0";
+  pantallaAnterior.innerHTML = "0";
+  operaciones = "";
 };
 
 const sumar = () => {
-  console.log("ENTRO");
-  const dataAnterior = pantalla.innerHTML;
+  const dataAnterior = pantallaActual.innerHTML;
   if (dataAnterior.charAt(0) === "0") return;
 
-  const valoresActuales =
-    pantalla.innerHTML.length != 0 && Number(pantalla.innerHTML);
+  operaciones =
+    operaciones === ""
+      ? `${operaciones}${dataAnterior}`
+      : `${operaciones}+${dataAnterior}`;
 
-  console.log(valoresActuales);
+  resultado = eval(operaciones);
 
-  if (valores[0] === undefined) {
-    valores[0] = valoresActuales;
-    pantalla.innerHTML = "";
-  } else if (valores[1] === undefined) {
-    valores[1] = valoresActuales;
-    pantalla.innerHTML = "";
+  if (operaciones.includes("+")) {
+    if (detectarSignoRepetido(operaciones)) {
+      console.log("1 signos");
+      pantallaAnterior.innerText = `${resultado}+${operaciones}`;
+    } else {
+      console.log("2 signos");
+      pantallaAnterior.innerText = operaciones;
+      pantallaActual.innerText = resultado;
+      operaciones = resultado;
+    }
   } else {
-    valores[2] = valores[0] + valores[1];
-    pantalla.innerHTML = valores[2];
+    pantallaAnterior.innerText = operaciones;
+    pantallaActual.innerText = "0";
   }
 
-  console.log(valores[0], valores[1], valores[2]);
+  // if (valores[0] === undefined) {
+  //   valores[0] = valoresActuales;
+  //   pantalla.innerHTML = "";
+  // } else if (valores[1] === undefined) {
+  //   valores[1] = valoresActuales;
+  //   pantalla.innerHTML = "";
+  // } else {
+  //   valores[2] = valores[0] + valores[1];
+  //   pantalla.innerHTML = valores[2];
+  // }
+
+  // console.log(valores[0], valores[1], valores[2]);
 };
 
-const resultado = () => {};
+detectarSignoRepetido("");
 
 const agregarMetodos = () => {
   for (let i = 0; i < 20; i++) {
@@ -49,8 +94,14 @@ const agregarMetodos = () => {
         agregarNumero(Number(botones[i].innerHTML));
       });
     }
-    if (botones[i].innerHTML.toString() == "+") {
+    if (botones[i].innerHTML.toString() === "+") {
       botones[i].addEventListener("click", sumar);
+    }
+    if (botones[i].innerHTML.toString() === "C") {
+      botones[i].addEventListener("click", limpiar);
+    }
+    if (botones[i].innerHTML.toString() === "CE") {
+      botones[i].addEventListener("click", limpiarCompleto);
     }
   }
 };
